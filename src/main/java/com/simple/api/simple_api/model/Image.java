741563d1e1ab22629497dfcc6abfbcf003a617e3
arif-rizal1122 <rizallarif32@gmail.com>
2025-02-27 14:17:13 +0800

    build entity table category, product and image

---
+++ src/main/java/com/simple/api/simple_api/model/Image.java
@@ -0,0 +1,42 @@
+package com.simple.api.simple_api.model;
+
+
+import java.sql.Blob;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Image {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+
+    private String fileType;
+
+    @Lob
+    private Blob image;
+
+    private String downloadURL;
+
+    @ManyToOne()
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
