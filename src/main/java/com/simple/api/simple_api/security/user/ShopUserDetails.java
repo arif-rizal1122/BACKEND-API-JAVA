8d45d80f2be215a0cd80ad98c02cc41f2202af3f
arif-rizal1122 <rizallarif32@gmail.com>
2025-03-11 09:37:20 +0800

    impl entity security package for UserDetails

---
+++ src/main/java/com/simple/api/simple_api/security/user/ShopUserDetails.java
@@ -0,0 +1,55 @@
+package com.simple.api.simple_api.security.user;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class ShopUserDetails implements UserDetails{
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return UserDetails.super.isEnabled();
+    }
+    
+
+    
+
+}
---
+++ src/main/java/com/simple/api/simple_api/security/user/UserDetailsService.java
@@ -0,0 +1,5 @@
+package com.simple.api.simple_api.security.user;
+
+public class UserDetailsService {
+    
+}
