517bcb97fba1cc8503b27beff34e5abfdaa4a440
arif-rizal1122 <rizallarif32@gmail.com>
2025-03-15 14:15:53 +0800

    implementing the login endpoint

--- src/main/java/com/simple/api/simple_api/controller/AuthController.java
+++ src/main/java/com/simple/api/simple_api/controller/AuthController.java
@@ -1,5 +1,53 @@
 package com.simple.api.simple_api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simple.api.simple_api.dto.request.LoginRequest;
+import com.simple.api.simple_api.dto.response.ApiResponse;
+import com.simple.api.simple_api.dto.response.JwtResponse;
+import com.simple.api.simple_api.security.jwt.JwtUtils;
+import com.simple.api.simple_api.security.user.ShopUserDetails;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/auth")
 public class AuthController {
+
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse> login(@Valid LoginRequest request){
+        
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate
+                    (new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = jwtUtils.generateTokenForUser(authentication);        
+            ShopUserDetails userDetails = (ShopUserDetails) authentication.getPrincipal();
+            JwtResponse jwtResponse = new JwtResponse(userDetails.getId(), jwt);
+
+            return ResponseEntity.ok(new ApiResponse("Login Success!!", jwtResponse));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse("username or email is wrong", null));
+        }
+
+    }
     
 }
